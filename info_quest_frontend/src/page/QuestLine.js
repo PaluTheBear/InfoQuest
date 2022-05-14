83b2b41b4c4cd22dfb4828df67f1116d813aaf5d
ThisCakeIsALie <37264026+ThisCakeIsALie@users.noreply.github.com>
2022-05-14 15:06:37 +0200

    QUestLine stuff

--- info_quest_frontend/src/page/QuestLine.js
+++ info_quest_frontend/src/page/QuestLine.js
@@ -2,12 +2,21 @@ import React from "react";
 import { useState, useEffect } from "react";
 import './QuestLine.css';
 import { GiBookmarklet } from 'react-icons/gi';
+import { getQuest } from "../util/RestHandler";
 
 const QuestLine = ({ questLine }) => {
+    const [quests, setQuests] = useState([])
+
+    useEffect(() => {
+        const quests = Promise.all(questLine.quests.map(getQuest));
+
+        quests.then(setQuests)
+    }, [ questLine ]);
+
     return (
         <div className='quest-line'>
-            {questLine.quests.map(quest => (
-                <div className='quest' key={quest}>
+            {quests.map(quest => (
+                <div className='quest' key={quest.id}>
                     <GiBookmarklet />
                 </div>
             ))}
--- info_quest_frontend/src/util/RestHandler.js
+++ info_quest_frontend/src/util/RestHandler.js
@@ -1,27 +1,27 @@
 import {questEndpoint, questsEndpoint, userInfoEndpoint} from "./constants";
 
 const getQuestlines = async () => {
+    return getMockQuestlines();
     const json = await fetch(questsEndpoint)
         .then(response => response.json())
         .catch(err => console.log(err));
     // return json
-    return getMockQuestlines();
 }
 
 const getUserInfo = async () => {
+    return getMockUserInfo();
     const json = await fetch(userInfoEndpoint)
         .then(response => response.json())
         .catch(err => console.log(err));
     // return json
-    return getMockUserInfo();
 }
 
 const getQuest = async (id) => {
+    return getMockQuest();
     const json = await fetch(`${questEndpoint}/${id}/`)
         .then(response => response.json())
         .catch(err => console.log(err));
     // return json
-    return getMockQuest();
 }
 
 const getMockQuestlines = () => [
@@ -35,6 +35,6 @@ const getMockQuestlines = () => [
     }
 ];
 const getMockUserInfo = () => [{id: 0, progress: 5}, {id: 2, progress: 2}];
-const getMockQuest = () => ({id: "id", title: "Questtitle", description: "Description stiudashakjsdaskjd"});
+const getMockQuest = () => ({id: Math.random(), title: "Questtitle", description: "Description stiudashakjsdaskjd"});
 
 export {getUserInfo, getQuest, getQuestlines}
